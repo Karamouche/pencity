77c635b89b587b365a759276ad2edb3296a15f4f
Karamouche <hugo4ibt@gmail.com>
2023-12-06 18:46:26 +0100

    add multithreading for data building

--- ml/train_processor.py
+++ ml/train_processor.py
@@ -7,6 +7,8 @@ from PIL import Image
 import random as rd
 import numpy as np
 import cv2
+from concurrent.futures import ThreadPoolExecutor
+
 
 from build_dataset import build_dataset
 from misc import from_coords_to_yolo, save_tensors
@@ -29,11 +31,15 @@ def train_processor(
         # seed to reproduce the same batch
         if seed != 0:
             rd.seed(seed)
-        for _ in tqdm(range(amount), desc="Building batch"):
-            images = dataset.shuffle().select(range(N_ITEMS))
-            # if there not at list one item per class, reshuffle and retry
-            while len(set([img["label"] for img in images])) < len(class_names):
-                images = dataset.shuffle().select(range(N_ITEMS))
+
+        def get_random_drawings() -> List[dict]:
+            picked = dataset.shuffle().select(range(N_ITEMS))
+            while len(set([item["label"] for item in picked])) < N_ITEMS / 2:
+                picked = dataset.shuffle().select(range(N_ITEMS))
+            return picked
+
+        def create_image() -> np.ndarray:
+            images = get_random_drawings()
             background = np.zeros((BACKGROUND_SIZE, BACKGROUND_SIZE))
             list_of_tensors = []
             for img in images:
@@ -61,8 +67,20 @@ def train_processor(
                     img, x, y, label, BACKGROUND_SIZE, class_names
                 )
                 list_of_tensors.append(yolo_tensor)
-            batch_images.append(background)
-            batch_tensors.append(torch.stack(list_of_tensors))
+            return background, torch.stack(list_of_tensors)
+
+        with ThreadPoolExecutor(max_workers=16) as executor:
+            with tqdm(total=amount, desc=f"Building batch") as pbar:
+                futures = []
+                for _ in range(amount):
+                    future = executor.submit(create_image)
+                    futures.append(future)
+
+                for future in futures:
+                    background, list_of_tensors = future.result()
+                    batch_images.append(background)
+                    batch_tensors.append(list_of_tensors)
+                    pbar.update(1)  # Update the progress bar
         return batch_images, batch_tensors
 
     # build and save dataset
@@ -81,4 +99,4 @@ def train_processor(
 
 if __name__ == "__main__":
     train_set, test_set, labels = build_dataset()
-    train_processor(train_set, test_set, labels, dataset_size=4096, seed=42)
+    train_processor(train_set, test_set, labels, dataset_size=4096)
