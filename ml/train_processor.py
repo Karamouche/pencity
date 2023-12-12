9643247fb478bd86d20a2cf94e1b33f700638fe6
Karamouche <hugo4ibt@gmail.com>
2023-12-12 14:27:38 +0100

    rework train_preprocess following lorelia's logic

--- ml/train_processor.py
+++ ml/train_processor.py
@@ -20,27 +20,62 @@ def train_processor(
     test_set: Dataset,
     class_names: List[str],
     BACKGROUND_SIZE: int = 416,
-    dataset_size=512,
+    amount_per_label=3000,
     seed=0,
 ):
     def build_batch(
-        dataset: Dataset, amount: int = 8, seed: int = 0
+        dataset: Dataset, amount_per_label: int, seed: int = 0
     ) -> Tuple[List[np.ndarray], List[torch.Tensor]]:
-        N_ITEMS = 16
         batch_images = []
         batch_tensors = []
         # seed to reproduce the same batch
         if seed != 0:
             rd.seed(seed)
 
-        def get_random_drawings() -> List[dict]:
-            picked = dataset.shuffle(keep_in_memory=True).select(range(N_ITEMS))
-            while len(set([item["label"] for item in picked])) < N_ITEMS / 2:
-                picked = dataset.shuffle(keep_in_memory=True).select(range(N_ITEMS))
-            return picked
+        def create_image_groups(max_images_per_group=16, min_images_per_group=8):
+            label_images = {i: [] for i in range(len(class_names))}
+            print(f"Taille du dataset: {len(dataset)}")
+            picking_pbar = tqdm(
+                range(amount_per_label * len(class_names)),
+                desc=f"Picking {amount_per_label} drawings per label",
+            )
+            for item in dataset:
+                if len(label_images[item["label"]]) < amount_per_label:
+                    label_images[item["label"]].append(item)
+                    picking_pbar.update(1)
+                # Stop if all images have been picked
+                if all(
+                    len(images) >= amount_per_label for images in label_images.values()
+                ):
+                    break
+            picking_pbar.close()
 
-        def create_image() -> np.ndarray:
-            images = get_random_drawings()
+            # Flatten the list and shuffle
+            print("Shuffling images")
+            all_images = [image for images in label_images.values() for image in images]
+            rd.shuffle(all_images)
+
+            # Create groups of images
+            print("Creating groups of images")
+            image_groups = []
+            groups_pbar = tqdm(range(len(all_images)))
+            while len(all_images) >= min_images_per_group:
+                group_size = rd.randint(
+                    min_images_per_group, min(max_images_per_group, len(all_images))
+                )
+                group = []
+                # pop images from the list and add them to the group
+                for _ in range(group_size):
+                    group.append(all_images.pop())
+
+                # add the group to the list of groups
+                image_groups.append(group)
+                groups_pbar.update(group_size)
+            groups_pbar.close()
+
+            return image_groups
+
+        def create_image(images) -> np.ndarray:
             background = np.zeros((BACKGROUND_SIZE, BACKGROUND_SIZE))
             list_of_tensors = []
             for img in images:
@@ -70,44 +105,37 @@ def train_processor(
                 list_of_tensors.append(yolo_tensor)
             return background, torch.stack(list_of_tensors)
 
-        with ThreadPoolExecutor() as executor:
-            with tqdm(total=amount, desc=f"Building batch") as pbar:
-                futures = []
-                for _ in range(amount):
-                    future = executor.submit(create_image)
-                    futures.append(future)
-
-                for future in futures:
-                    background, list_of_tensors = future.result()
-                    batch_images.append(background)
-                    batch_tensors.append(list_of_tensors)
-                    pbar.update(1)  # Update the progress bar
-                    gc.collect()
+        image_groups = create_image_groups()
+        print(f"Nombre de groupes : {len(image_groups)}")
+        for groups in tqdm(image_groups, desc="Creating images"):
+            background, list_of_tensors = create_image(groups)
+            batch_images.append(background)
+            batch_tensors.append(list_of_tensors)
         return batch_images, batch_tensors
 
     # build and save dataset
     print("Building train data")
     train_batch_images, train_batch_tensors = build_batch(
-        train_set, amount=dataset_size, seed=seed
+        train_set, amount_per_label=amount_per_label, seed=seed
     )
     save_tensors(train_batch_images, train_batch_tensors, split="train")
-    print("Building test data")
+    print("\nBuilding test data")
     test_batch_images, test_batch_tensors = build_batch(
-        test_set, amount=int(dataset_size / 4), seed=seed
+        test_set, amount_per_label=int(amount_per_label / 4), seed=seed
     )
     save_tensors(test_batch_images, test_batch_tensors, split="test")
-    print("Dataset built")
+    print("\nDataset built")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-s",  # --dataset_size
-        "--dataset_size",
+        "--size_per_label",
         type=int,
         default=512,
-        help="Size of the dataset to build",
+        help="How many images per label in the dataset",
     )
     args = parser.parse_args()
     train_set, test_set, labels = build_dataset()
-    train_processor(train_set, test_set, labels, dataset_size=args.dataset_size)
+    train_processor(train_set, test_set, labels, amount_per_label=args.size_per_label)
