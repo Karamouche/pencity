1ac373c09d604f8645b678b552f71b3dbddf9a57
Karamouche <hugo4ibt@gmail.com>
2023-12-14 14:59:22 +0100

    add a confidence option

--- ml/inference.py
+++ ml/inference.py
@@ -3,6 +3,8 @@ import torch
 import argparse
 import time
 
+CONFIDENCE_TRESHOLD = 0.7
+
 
 def load_model(weight_path, name="ultralytics/yolov5"):
     model = torch.hub.load(name, "custom", path=weight_path)
@@ -11,15 +13,15 @@ def load_model(weight_path, name="ultralytics/yolov5"):
 
 def preprocess_cam(frame):
     # resize to 640x640
-    frame = cv2.resize(frame, (416, 416))
+    frame = cv2.resize(frame, (640, 640))
     # to grayscaleYOLOv5
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # to black and white
     _, frame = cv2.threshold(frame, 115, 255, cv2.THRESH_BINARY)
     # reverse color
-    # frame = cv2.bitwise_not(frame)
+    frame = cv2.bitwise_not(frame)
     # remove black noise
-    frame = cv2.morphologyEx(frame, cv2.MORPH_OPEN, (5, 5))
+    frame = cv2.morphologyEx(frame, cv2.MORPH_OPEN, (3, 3))
 
     return frame
 
@@ -47,6 +49,12 @@ if __name__ == "__main__":
 
         # Perform inference on the frame
         results = model(frame)
+        print(results.pred)
+        # Apply confidence threshold
+        confiend_elements = [
+            good for good in results.pred[0].tolist() if good[4] > CONFIDENCE_TRESHOLD
+        ]
+        results.pred[0] = torch.tensor(confiend_elements)
 
         # Display the results
         cv2.imshow("Drawlo", results.render()[0])
