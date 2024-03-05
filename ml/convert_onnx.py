cdb10af45861d9339835f85ae079835564e40fba
Lorelia <lorelia.latarche@gmail.com>
2024-03-05 16:37:56 +0100

    test onnx python

--- ml/convert_onnx.py
+++ ml/convert_onnx.py
@@ -3,4 +3,7 @@ from ultralytics import YOLO
 
 model = YOLO('runs/detect/test_90000/weights/best.pt')
 
-model.export(format="onnx", imgsz=[640, 640], opset=12)
+model.export(format="onnx", imgsz=[640, 640], opset=10)
+
+#launch the code from the racine folder
+# get the onnx file in the folder of the model
